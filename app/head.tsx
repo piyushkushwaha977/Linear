cc814cb756ce8ca44a1de6b5a8e36f07ae2235b4
piyushkushwaha977 <piyushkushwaha977@gmail.com>
2024-09-03 05:04:23 +0530

    first Commit of Linear Website

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <title>Linear by Piyush</title>
+      <meta
+        name="description"
+        content="Rebuilding Linear.app's website with Next.js, Tailwind."
+      />
+      <meta name="robots" content="noindex" />
+    </>
+  );
+}
